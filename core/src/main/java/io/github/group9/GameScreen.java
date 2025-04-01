08555903e3dd111065332d2985a75320b4dfd320
Florentin Mustafa <flmus23@student.sdu.dk>
2025-04-01 13:28:41 +0200

    Changes by christoffersteentoft

---
+++ core/src/main/java/io/github/group9/GameScreen.java
@@ -0,0 +1,18 @@
+package io.github.group9;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import java.util.ServiceLoader;
+
+
+public class GameScreen implements Screen {
+
+    @Override
+    public void show() {
+    }
+}
